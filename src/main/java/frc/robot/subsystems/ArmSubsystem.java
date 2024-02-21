5e9d0f7f7b67f8e0131cec48211d2f9510ab421d
Sumedh-Pisipati <800025476@fjuhsd.org>
2024-02-20 16:07:08 -0800

    Test

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,23 +1,28 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import frc.robot.Constants.OperatorConstants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
+    private CANSparkMax armMotor1;
+    private CANSparkMax armMotor2;
     public ArmSubsystem() {
-        this.ARM_MOTOR_1_PORT = new CANSparkMax(PortConstraints.CLAW_ROTATE_PORT, MotorType.kBrushless);
-        this.ARM_MOTOR_2_PORT = new CANSparkMax(PortConstraints.CLAW_ROTATE_PORT, MotorType.kBrushless);
-        public void setSpeedMotor1(double speed) {
-            this.ARM_MOTOR_1_PORT.set(speed);
+        armMotor1 = new CANSparkMax(OperatorConstants.ARM_MOTOR_1_PORT, MotorType.kBrushless);
+        armMotor2 = new CANSparkMax(OperatorConstants.ARM_MOTOR_2_PORT, MotorType.kBrushless);
+    }
+        public void setSpeedMotor1(double speed){
+            this.armMotor1.set(speed);
         }
         public void setSpeedMotor2(double speed) {
-            this.ARM_MOTOR_2_PORT.set(speed);
+            this.armMotor2.set(speed);
         }
         public void setSpeedBoth(double speed1, double speed2) {
-            this.ARM_MOTOR_1_PORT.set(speed1);
-            this.ARM_MOTOR_2_PORT.set(speed2);
+            this.armMotor1.set(speed1);
+            this.armMotor2.set(speed2);
         }
-    }
+    
 
 
 }
